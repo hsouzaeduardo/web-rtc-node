379ae765918201f4a14967486e6c329cf1a0aac7
Henrique Eduardo souza <hsouza.eduardo@gmail.com>
2025-06-18 17:16:32 -0300

    fix/envport

--- server.js
+++ server.js
@@ -16,4 +16,8 @@ io.on("connection", socket => {
   socket.on("ice", data => socket.broadcast.emit("ice", data));
 });
 
-server.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
+// server.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
